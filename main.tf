02d0781e5c196f9d7b17e465b59a51a7cfd90c27
root <root@ubuntu1804.localdomain>
2023-01-18 12:46:41 +0000

    change to us-west-1 and ubuntu 20.04

--- main.tf
+++ main.tf
@@ -19,18 +19,18 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-1"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+resource "aws_key_pair" "master-key" {
+  key_name   = "kube-master-key"
   public_key = file("/root/.ssh/id_rsa.pub")
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+resource "aws_instance" "kube-master" {
+  ami           = "ami-0fb6aae5c678c89fa"
   instance_type = "t2.micro"
-  key_name = "deployer-key"
+  key_name = "kube-master-key"
 
   tags = {
     Name = var.instance_name
